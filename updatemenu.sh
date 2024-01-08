b57b6a2171a640cb1729d06ce93d1ab32a1e41a0
Bambang Heriyanto <foreverwelearn@gmail.com>
2024-01-08 21:44:52 +0700

    Update updatemenu.sh

--- updatemenu.sh
+++ updatemenu.sh
@@ -20,20 +20,35 @@ export HERROR="[${RED} ERROR ${NC}]"
 export HINFO="[${YELLOW} INFO ${NC}]"
 export HOK="[${GREEN} OK ${NC}]"
 
+echo -e "${GREEN}Deleting Old Menu ${NC}"
 rm -fr /usr/bin/autoreboot
+echo -e "${HOK} Delete Auto Reboot"
 rm -fr /usr/bin/clearlog
+echo -e "${HOK} Delete Clear Log"
 rm -fr /usr/bin/dns
+echo -e "${HOK} Delete DNS"
 rm -fr /usr/bin/limit-speed
+echo -e "${HOK} Delete Limit Speed"
 rm -fr /usr/bin/menu
+echo -e "${HOK} Delete Main Menu"
 rm -fr /usr/bin/menu-ssh
+echo -e "${HOK} Delete SSH"
 rm -fr /usr/bin/netf
+echo -e "${HOK} Delete NetFlix Check"
 rm -fr /usr/bin/cek-ram
 rm -fr /usr/bin/restart
+echo -e "${HOK} Delete Restart"
 rm -fr /usr/bin/running
+echo -e "${HOK} Delete Running"
 rm -fr /usr/bin/cek-speed
+echo -e "${HOK} Delete Speedtest"
 rm -fr /usr/bin/tendang
+echo -e "${HOK} Delete AutoKill"
 rm -fr /usr/bin/usernew
+echo -e "${HOK} Delete UserNew"
 rm -fr /usr/bin/wbm
+echo -e "${HOK} Delete Webmin"
+sleep 2
 
 # // Download Data
 echo -e "${GREEN}Download Data${NC}"
@@ -51,6 +66,8 @@ wget -q -O /usr/bin/cek-speed "https://raw.githubusercontent.com/asheeka/QiaTunn
 wget -q -O /usr/bin/tendang "https://raw.githubusercontent.com/asheeka/QiaTunnel/main/tendang.sh"
 wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/asheeka/QiaTunnel/main/usernew.sh"
 wget -q -O /usr/bin/wbm "https://raw.githubusercontent.com/asheeka/QiaTunnel/main/webmin.sh"
+echo -e "${HOK} Download data finished"
+sleep 2
 
 chmod +x /usr/bin/autoreboot
 chmod +x /usr/bin/clearlog
@@ -66,7 +83,7 @@ chmod +x /usr/bin/cek-speed
 chmod +x /usr/bin/tendang
 chmod +x /usr/bin/usernew
 chmod +x /usr/bin/wbm
-echo -e "${GREEN}Update Finish..${NC}"
-sleep 1
-read -p "$( echo -e "Press ${orange}[ ${NC}${green}Enter${NC} ${CYAN}]${NC} For Reboot") "
+echo -e "${HINFO}Update Finish..${NC}"
+sleep 2
+read -p "$( echo -e "Press ${YELLOW}[ ${NC}${GREEN}Enter${NC} ${CYAN}]${NC} For Reboot") "
 reboot
